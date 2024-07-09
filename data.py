2bfde7785f83cb27994d8a25d5632c486617de46
KangHoML <kanghokhkh1@gmail.com>
2024-07-09 14:47:33 +0900

    data sampling

--- data.py
+++ data.py
@@ -1,8 +1,12 @@
 import os
 import json
+import random
 
 from datasets import Dataset, DatasetDict
 from huggingface_hub import HfApi
+
+SEED = 42
+random.seed(SEED)
 
 def make_dataset(root, train=True):
     dataset = []
@@ -25,17 +29,29 @@ def make_dataset(root, train=True):
     # dataset으로 변환하여 반환
     return Dataset.from_list(dataset)
 
+def sampling(train_dataset, sample_sizes):
+    sampled_datasets = {}
+    for size in sample_sizes:
+        sampled_train = train_dataset.shuffle(seed=SEED).select(range(size))
+        sampled_datasets[f"train_{size//1000}k"] = sampled_train
+    return sampled_datasets
+
 if __name__ == "__main__":
     # 데이터셋 구성
     root = "../datasets/KoreanError"
     train_dataset, val_dataset = make_dataset(root, train=True), make_dataset(root, train=False)
 
+    # 샘플링
+    sample_sizes = [50000, 80000, 100000]
+    train_sampled = sampling(train_dataset, sample_sizes)
+
     # Dictionary 형태로 변환
     data_dict = DatasetDict({
         "train": train_dataset,
-        "val": val_dataset
+        "val": val_dataset,
+        **train_sampled
     })
-
+    
     # HuggingFace에 로그인
     api = HfApi(token=os.environ.get("HF_TOKEN"))
 
